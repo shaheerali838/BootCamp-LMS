2e625472a28485be6f85e2d3f8687d1592bb4cb1
Mustafakamal10 <murtazakamalkamalhussain@gmail.com>
2026-08-07 00:04:45 +0500

    Add initial layout components: Footer, Navbar, Sidebar, and common components: Breadcrumb; create authentication pages: Login, Signup, ForgetPassword, ResetPassword; add AppRoutes and ProfileSettings

---
+++ Frontend/src/components/common/Breadcrumb.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Breadcrumb() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Breadcrumb
---
+++ Frontend/src/pages/routes/AppRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppRoutes() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppRoutes
