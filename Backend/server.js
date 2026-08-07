6b4ab621b08d43fdf834b5eb344ab6cdb891a285
Shaheer Ali <shaheer838838@gmail.com>
2026-08-07 11:15:03 +0500

    Implement initial backend structure with Express, MongoDB connection, and essential middleware

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,13 @@
-import express from "express";
+import dotenv from "dotenv";
+import app from "./utils/app.js";
+import connectDB from "./config/db.js";
 
-const app = express();
+dotenv.config();
 
-app.use(cors());
+await connectDB();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
