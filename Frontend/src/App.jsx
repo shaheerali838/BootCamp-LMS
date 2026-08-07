972ac3ab0de434cda05663131dd40d0c5469670d
Shaheer Ali <shaheer838838@gmail.com>
2026-08-07 12:23:54 +0500

    fix: resolve merge conflicts and clean up imports in App and main components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,19 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import SignUpPage from './pages/SignUpPage'
-import LoginPage from './pages/LoginPage'
-import LoginOutPage from './pages/LoginOutPage'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import LoginOutPage from "./pages/LoginOutPage";
 
 const App = () => {
   return (
     <div>
-
       <Routes>
-        <Route path='/sign-up' element={<SignUpPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/login-out' element={<LoginOutPage/>}/>
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login-out" element={<LoginOutPage />} />
       </Routes>
     </div>
-=======
-import './App.css'
+  );
+};
 
-function App() {
-  
-
-  return (
-    <>
-      
-    </>
->>>>>>> origin/Frontend/mustafa-kamal
-  )
-}
-
-export default App
+export default App;
