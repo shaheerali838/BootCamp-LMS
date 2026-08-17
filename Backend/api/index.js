546e7b7da4aa83f63e751abb7f992e8ec91966de
Shaheer Ali <shaheer838838@gmail.com>
2026-08-17 13:21:54 +0500

    feat(backend): automatically seed initial Super Admin and Admin on Vercel deployment

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,10 +1,17 @@
 import "dotenv/config";
 import app from "../app.js";
 import connectDB from "../config/db.js";
+import seedAdmin from "../seed/seedAdmin.js";
+
+let isSeeded = false;
 
 export default async function handler(req, res) {
   try {
     await connectDB();
+    if (!isSeeded) {
+      await seedAdmin();
+      isSeeded = true;
+    }
     return app(req, res);
   } catch (error) {
     console.error("Vercel Serverless Handler DB Error:", error);
